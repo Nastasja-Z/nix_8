b04eccb54648636a260d2be860b8d47e5ca877ee
egorfuntusov <funtushan@gmail.com>
2021-12-04 20:01:00 +0200

    add review

--- hw_4_logs_and_test/src/main/java/ua/com/alevel/LogsAndTestMain.java
+++ hw_4_logs_and_test/src/main/java/ua/com/alevel/LogsAndTestMain.java
@@ -3,6 +3,7 @@ package ua.com.alevel;
 import ua.com.alevel.view.WorkerTaskView;
 
 public class LogsAndTestMain {
+
     public static void main(String[] args) {
         new WorkerTaskView().run();
     }
