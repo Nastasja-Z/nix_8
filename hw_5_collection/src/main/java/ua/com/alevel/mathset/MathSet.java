fc7501348f97e4823a10ecfc43e699a3343c587c
Nastasja-Z <anastasia.zimnenko@gmail.com>
2021-12-14 03:25:56 +0200

    add hw_5 some changes

--- hw_5_collection/src/main/java/ua/com/alevel/mathset/MathSet.java
+++ hw_5_collection/src/main/java/ua/com/alevel/mathset/MathSet.java
@@ -1,6 +1,5 @@
 package ua.com.alevel.mathset;
 
-import java.net.DatagramPacket;
 import java.util.Arrays;
 
 public class MathSet<NUM extends Number> {
@@ -139,12 +138,14 @@ public class MathSet<NUM extends Number> {
 
     public void sortDesc() {
         Number temp;
-        for (int i = 1; i < currentLastIndex + 1; i++) { //added +1
+        for (int i = 1; i < currentLastIndex + 1; i++) {
             for (int j = i; j > 0; j--) {
-                if (numbers[j].doubleValue() > numbers[j - 1].doubleValue()) {
-                    temp = numbers[j];
-                    numbers[j] = numbers[j - 1];
-                    numbers[j - 1] = temp;
+                if (numbers[j] != null) {
+                    if (numbers[j].doubleValue() > numbers[j - 1].doubleValue()) {
+                        temp = numbers[j];
+                        numbers[j] = numbers[j - 1];
+                        numbers[j - 1] = temp;
+                    }
                 }
             }
         }
@@ -154,10 +155,12 @@ public class MathSet<NUM extends Number> {
         Number temp;
         for (int i = firstIndex; i < lastIndex; i++) {
             for (int j = i; j > firstIndex; j--) {
-                if (numbers[j].doubleValue() > numbers[j - 1].doubleValue()) {
-                    temp = numbers[j];
-                    numbers[j] = numbers[j - 1];
-                    numbers[j - 1] = temp;
+                if (numbers[j] != null) {
+                    if (numbers[j].doubleValue() > numbers[j - 1].doubleValue()) {
+                        temp = numbers[j];
+                        numbers[j] = numbers[j - 1];
+                        numbers[j - 1] = temp;
+                    }
                 }
             }
         }
@@ -166,7 +169,7 @@ public class MathSet<NUM extends Number> {
     public void sortDesc(Number value) {
         int index = -1;
         for (int i = 0; i < currentLastIndex; i++) { //???
-            if (numbers[i].equals(value)) {
+            if (numbers[i].doubleValue() == value.doubleValue()) {
                 index = i;
             }
         }
@@ -177,10 +180,12 @@ public class MathSet<NUM extends Number> {
         Number temp;
         for (int i = 1; i < currentLastIndex + 1; i++) {
             for (int j = i; j > 0; j--) {
-                if (numbers[j].doubleValue() < numbers[j - 1].doubleValue()) {
-                    temp = numbers[j];
-                    numbers[j] = numbers[j - 1];
-                    numbers[j - 1] = temp;
+                if (numbers[j] != null) {
+                    if (numbers[j].doubleValue() < numbers[j - 1].doubleValue()) {
+                        temp = numbers[j];
+                        numbers[j] = numbers[j - 1];
+                        numbers[j - 1] = temp;
+                    }
                 }
             }
         }
@@ -190,10 +195,12 @@ public class MathSet<NUM extends Number> {
         Number temp;
         for (int i = firstIndex; i < lastIndex + 1; i++) {
             for (int j = i; j > firstIndex; j--) {
-                if (numbers[j].doubleValue() < numbers[j - 1].doubleValue()) {
-                    temp = numbers[j];
-                    numbers[j] = numbers[j - 1];
-                    numbers[j - 1] = temp;
+                if (numbers[j] != null) {
+                    if (numbers[j].doubleValue() < numbers[j - 1].doubleValue()) {
+                        temp = numbers[j];
+                        numbers[j] = numbers[j - 1];
+                        numbers[j - 1] = temp;
+                    }
                 }
             }
         }
@@ -201,7 +208,7 @@ public class MathSet<NUM extends Number> {
 
     public void sortAsc(Number value) {
         int index = -1;
-        for (int i = 0; i < currentLastIndex; i++) { ///???????
+        for (int i = 0; i < currentLastIndex; i++) {
             if (numbers[i].equals(value)) {
                 index = i;
             }
@@ -221,7 +228,7 @@ public class MathSet<NUM extends Number> {
     }
 
     public Number getMax() {
-        Number max = -99999999;
+        Number max = Integer.MIN_VALUE;
         for (int i = 0; i < currentLastIndex; i++) {
             if (numbers[i].doubleValue() > max.doubleValue()) {
                 max = numbers[i];
@@ -231,7 +238,7 @@ public class MathSet<NUM extends Number> {
     }
 
     public Number getMin() {
-        Number min = 10000000;
+        Number min = Integer.MAX_VALUE;
         for (int i = 0; i < currentLastIndex; i++) {
             if (numbers[i].doubleValue() < min.doubleValue()) {
                 min = numbers[i];
@@ -265,29 +272,73 @@ public class MathSet<NUM extends Number> {
         }
         return valueOfMediana;
     }
- /*
 
-
-
-    public Number[] toArray(){
-
+    public void print(String s) {
+        System.out.print("\nYOUR " + s + " MATH SET  ");
+        System.out.println(Arrays.toString(toArray()));
+        System.out.println();
     }
 
-    public Number[] toArray(int firstIndex, int lastIndex){
-
+    public Number[] toArray() {
+        Number[] newArr = new Number[currentLastIndex];
+        for (int i = 0; i < currentLastIndex; i++) {
+            newArr[i] = numbers[i];
+        }
+        return newArr;
     }
 
-    public MathSet cut(int firstIndex, int lastIndex){
-
+    public Number[] toArray(int firstIndex, int lastIndex) {
+        Number[] newArr = new Number[lastIndex - firstIndex];
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = numbers[i + firstIndex];
+        }
+        return newArr;
     }
 
-    public void clear(){
-
+    public MathSet<Number> cut(int firstIndex, int lastIndex) {
+        MathSet<Number> mathSet = new MathSet(lastIndex - firstIndex);
+        if (firstIndex >= 0 && firstIndex < lastIndex && lastIndex < currentLastIndex) {
+            for (int i = 0; i < firstIndex; i++) {
+                mathSet.add(numbers[i]);
+            }
+            for (int i = lastIndex + 1; i < currentLastIndex; i++) {
+                mathSet.add(numbers[i]);
+            }
+            return mathSet;
+        } else {
+            throw new IndexOutOfBoundsException("INDEX OUT OF BONDS");
+        }
     }
 
-    public void clear(Number[] numbers){
+    public void clear() {
+        Number[] cleared = new Number[INITIAL_SIZE];
+        currentLastIndex = 0;
+        sizeOfArray = INITIAL_SIZE;
+        numbers = cleared;
+    }
 
-    }*/
+    public void clear(Number[] numbersi) {
+        Number[] newArr = new Number[currentLastIndex + 1];
+        int last = 0;
+        for (int i = 0; i < currentLastIndex; i++) {
+            boolean exists = false;
+            for (int j = 0; j < numbersi.length; j++) {
+                if (numbers[i].equals(numbersi[j])) {
+                    exists = true;
+                    break;
+                }
+            }
+            if (exists) {
+                for (int j = i + 1; j < currentLastIndex; j++)
+                    numbers[j - 1] = numbers[j];
+                currentLastIndex--;
+                i--;
+                if (currentLastIndex >= 0)
+                    System.arraycopy(numbers, 0, newArr, 0, currentLastIndex);
+                numbers = newArr;
+            }
+        }
+    }
 
     private int getSizeOfDoubleArray(Number[]... numbers) {
         int temp = 0;
@@ -309,22 +360,28 @@ public class MathSet<NUM extends Number> {
 
     public void contains() {
         for (int i = 0; i < currentLastIndex; i++) {
-            boolean exists = false;
-            for (int j = 0; j < i; j++) {
-                if (numbers[i].equals(numbers[j])) {
-                    exists = true;
-                    break;
+            if(numbers[i]!=null){
+                boolean exists = false;
+                for (int j = 0; j < i; j++) {
+                    if (numbers[j] != null) {
+                        if (numbers[i].equals(numbers[j])) {
+                            exists = true;
+                            System.out.println("already exists");
+                            break;
+                        }
+                    }
                 }
-            }
-            Number[] newArr = new Number[sizeOfArray];
-            if (exists) {
-                for (int j = i + 1; j < currentLastIndex; j++)
-                    numbers[j - 1] = numbers[j];
-                currentLastIndex--;
-                i--;
-                if (currentLastIndex >= 0)
-                    System.arraycopy(numbers, 0, newArr, 0, currentLastIndex);
-                numbers = newArr;
+                Number[] newArr = new Number[sizeOfArray];
+                if (exists) {
+                    for (int j = i + 1; j < currentLastIndex; j++)
+                        numbers[j - 1] = numbers[j];
+                    currentLastIndex--;
+                    i--;
+                    if (currentLastIndex >= 0) {
+                        System.arraycopy(numbers, 0, newArr, 0, currentLastIndex);
+                    }
+                    numbers = newArr;
+                }
             }
         }
     }
@@ -336,14 +393,6 @@ public class MathSet<NUM extends Number> {
         return false;
     }
 
-    public void print(String s) {
-        System.out.print("\nYOUR"+s+" MATH SET  ");
-        for (int i = 0; i < currentLastIndex; i++) {
-            System.out.print(numbers[i]+" , ");
-        }
-        System.out.println();
-    }
-
     @Override
     public String toString() {
         return "MathSet{" +
@@ -352,4 +401,5 @@ public class MathSet<NUM extends Number> {
                 ", numbers=" + Arrays.toString(numbers) +
                 '}';
     }
+
 }
--- hw_5_collection/src/main/java/ua/com/alevel/view/MathSetRun.java
+++ hw_5_collection/src/main/java/ua/com/alevel/view/MathSetRun.java
@@ -1,24 +1,25 @@
 package ua.com.alevel.view;
 
+import org.decimal4j.util.DoubleRounder;
 import ua.com.alevel.mathset.MathSet;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.Scanner;
 
 public class MathSetRun {
 
-    private MathSet<Number> mathSet = new MathSet(new Number[]{1, 2, 44, 1.3, 3, 3, 7.1F});
-    private String currMS= "CURRENT";
+    private MathSet<Number> mathSet = new MathSet();
+    private String currMS = "CURRENT";
     private String helperMS = "HELPER";
 
     public void run() {
+        setMS(15, mathSet);
         mathSet.print(currMS);
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("\nSelect a number of action");
         String position;
         try {
             runNavigation();
@@ -30,51 +31,6 @@ public class MathSetRun {
         } catch (IOException e) {
             System.out.println("problem: = " + e.getMessage());
         }
-        /*MathSet<Number> mathSet = new MathSet<>();
-        System.out.println(mathSet);
-        mathSet = new MathSet<>(7);
-        System.out.println(mathSet);
-        Integer[] integer = new Integer[10];
-        for (int i = 0; i < integer.length; i++) {
-            integer[i] = i;
-        }
-        mathSet = new MathSet<>(integer);
-        System.out.println(mathSet);
-        Integer[][] int2 = new Integer[10][3];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 3; j++) {
-                int2[i][j] = i + j;
-            }
-        }
-        mathSet = new MathSet<>(int2);
-        System.out.println(mathSet);
-
-        MathSet<Number> mathSet1 = new MathSet<>(new Number[]{1, 2, 3});
-        mathSet = new MathSet<>(mathSet1);
-        System.out.println(mathSet);
-        MathSet<Number> floatMathSet = new MathSet<>(new Number[]{1.2F, 1.3F, 1.4F});
-
-        MathSet<Number> mathSet2 = new MathSet<>(mathSet, floatMathSet);
-        System.out.println(mathSet2);
-
-        System.out.println();
-        mathSet2.add(integer);
-        mathSet2.add(3.0);
-        mathSet2.join(mathSet);
-        mathSet2.join(mathSet, floatMathSet);
-        System.out.println(mathSet2);
-        mathSet1.add(1);
-        mathSet1.add(1.2F);
-        floatMathSet.add(1.3F);
-        mathSet1.add(1.3F);
-        floatMathSet.add(1);
-        mathSet2.intersection(mathSet1, floatMathSet);
-        System.out.println(mathSet2);
-        MathSet<Number> ms = new MathSet<>(new Number[]{1, 2, 3, 100, 50, 7});
-
-        System.out.println(ms);
-        Number index = ms.getMedian();
-        System.out.println(index);*/
     }
 
     private void runNavigation() {
@@ -84,17 +40,18 @@ public class MathSetRun {
         System.out.println("3 - join");
         System.out.println("4 - intersection");
         System.out.println("5 - sort desc");
-        System.out.println("0 - sort asc");
-        System.out.println("0 - get");
-        System.out.println("0 - get max");
-        System.out.println("0 - get min");
-        System.out.println("0 - average");
-        System.out.println("0 - median");
-        System.out.println("0 - to array");
-        System.out.println("0 - cut");
-        System.out.println("0 - clear");
+        System.out.println("6 - sort asc");
+        System.out.println("7 - get");
+        System.out.println("8 - get max");
+        System.out.println("9 - get min");
+        System.out.println("10 - average");
+        System.out.println("11 - median");
+        System.out.println("12 - to array (print)");
+        System.out.println("13 - cut");
+        System.out.println("14 - clear");
         System.out.println("0 - exit");
-        System.out.println();
+        mathSet.print(currMS);
+        System.out.println("\nSelect a number of action");
     }
 
     private void mathset(String position, BufferedReader reader) {
@@ -108,64 +65,235 @@ public class MathSetRun {
             case "3":
                 join(reader);
                 break;
+            case "4":
+                intersection(reader);
+                break;
+            case "5":
+                sortDesc(reader);
+                break;
+            case "6":
+                sortAsc(reader);
+                break;
+            case "7":
+                get(reader);
+                break;
+            case "8":
+                getMax(reader);
+                break;
+            case "9":
+                getMin(reader);
+                break;
+            case "10":
+                getAvg(reader);
+                break;
+            case "11":
+                getMedian(reader);
+                break;
+            case "12":
+                toArray(reader);
+                break;
+            case "13":
+                cut(reader);
+                break;
+            case "14":
+                clear(reader);
+                break;
             case "0":
                 System.exit(0);
         }
-        /*switch (position) {
-            case "1":
-                create(reader);
-                break;
-            case "2":
-                update(reader);
-                break;
-            case "3":
-                delete(reader);
-                break;
-            case "4":
-                findById(reader);
-                break;
-            case "5":
-                findAll(reader);
-                break;
-            case "0":
-                System.exit(0);
-        }*/
         runNavigation();
     }
 
-    private void join(BufferedReader reader) {
-        MathSet<Number> mathSetForJoin=new MathSet<Number>(new Number[]{1, 2, 4, 2.3, 3});
-        mathSetForJoin.print(helperMS);
+    private void clear(BufferedReader reader) {
+        System.out.println("\n1 - full clear, 2 - numbers");
+        String position;
+        try {
+            position = reader.readLine();
+            if (position.equals("1")) {
+                mathSet.clear();
+                System.out.println("math set is empty");
+            } else if (position.equals("2")) {
+                MathSet<Number> mathSetForClearing = new MathSet<>();
+                System.out.println("enter indexes of elements to remove them (enter '-1' to stop entering)");
+                int index = Integer.parseInt(reader.readLine());
+                while (index != -1) {
+                    if (index >= 0) {
+                        mathSetForClearing.add(mathSet.get(index));
+                    }
+                    index = Integer.parseInt(reader.readLine());
+                }
+                mathSetForClearing.print(helperMS);
+                mathSetForClearing.sortDesc();
+                mathSet.sortDesc();
+                mathSet.clear(mathSetForClearing.getNumbers());
+            }
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
 
-        //dalshe
+    private void cut(BufferedReader reader) {
+        try {
+            System.out.println("Enter first index");
+            int firstIndex = Integer.parseInt(reader.readLine());
+            System.out.println("Enter last index");
+            int lastIndex = Integer.parseInt(reader.readLine());
+            mathSet = mathSet.cut(firstIndex, lastIndex);
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
+
+    private void toArray(BufferedReader reader) {
+        System.out.println("\n1 - full math set, 2 - first ind/last index");
+        String position;
+        try {
+            position = reader.readLine();
+            if (position.equals("1")) {
+                mathSet.print(currMS);
+            } else if (position.equals("2")) {
+                System.out.println("Enter first index");
+                int firstIndex = Integer.parseInt(reader.readLine());
+                System.out.println("Enter last index");
+                int lastIndex = Integer.parseInt(reader.readLine());
+                if (firstIndex < lastIndex && firstIndex >= 0 && lastIndex < mathSet.toArray().length) {
+                    for (Number number : mathSet.toArray(firstIndex, lastIndex)) {
+                        System.out.print(number + " ");
+                    }
+                } else {
+                    throw new IndexOutOfBoundsException("YOUR INDEX IS OUT OF BOUNDS");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
+
+    private void getMedian(BufferedReader reader) {
+        System.out.println("median = " + mathSet.getMedian());
+    }
+
+    private void getAvg(BufferedReader reader) {
+        System.out.println("average = " + mathSet.getAverage());
+    }
+
+    private void getMin(BufferedReader reader) {
+        System.out.println("min = " + mathSet.getMin());
+    }
+
+    private void getMax(BufferedReader reader) {
+        System.out.println("max = " + mathSet.getMax());
+    }
+
+    private void get(BufferedReader reader) {
+        System.out.println("\nEnter index of element:");
+        Integer position;
+        try {
+            position = Integer.parseInt(reader.readLine());
+            System.out.println(mathSet.get(position));
+
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
+
+    private void sortAsc(BufferedReader reader) {
+        System.out.println("\n1 - full asc sort, 2 - first ind/last ind, 3 - by number");
+        String position;
+        try {
+            position = reader.readLine();
+            if (position.equals("1")) {
+                mathSet.sortAsc();
+            } else if (position.equals("2")) {
+                System.out.println("Enter first index");
+                int firstIndex = Integer.parseInt(reader.readLine());
+                System.out.println("Enter last index");
+                int lastIndex = Integer.parseInt(reader.readLine());
+                mathSet.sortAsc(firstIndex, lastIndex);
+            } else if (position.equals("3")) {
+                System.out.println("Enter number");
+                double number = Double.parseDouble(reader.readLine());
+                mathSet.sortAsc(number);
+            }
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
+
+    private void sortDesc(BufferedReader reader) {
+        System.out.println("\n1 - full desc sort, 2 - first ind/last ind, 3 - by number");
+        String position;
+        try {
+            position = reader.readLine();
+            if (position.equals("1")) {
+                mathSet.sortDesc();
+            } else if (position.equals("2")) {
+                System.out.println("Enter first index");
+                int firstIndex = Integer.parseInt(reader.readLine());
+                System.out.println("Enter last index");
+                int lastIndex = Integer.parseInt(reader.readLine());
+                mathSet.sortDesc(firstIndex, lastIndex);
+            } else if (position.equals("3")) {
+                System.out.println("Enter number");
+                double number = Double.parseDouble(reader.readLine());
+                mathSet.sortDesc(number);
+            }
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
+
+    private void intersection(BufferedReader reader) {
+        System.out.println("INTERSECTION OF TWO MATH SETS");
+        MathSet<Number> mathSetForIntersection = new MathSet<>();
+        setMS(10, mathSetForIntersection);
+        mathSetForIntersection.add(mathSet.get(1));
+        mathSetForIntersection.add(mathSet.get(0));
+        mathSetForIntersection.print(helperMS);
+        mathSet.intersection(mathSetForIntersection);
+    }
+
+    private void join(BufferedReader reader) {
+        System.out.println("JOIN OF TWO MATH SETS");
+        MathSet<Number> mathSetForJoin = new MathSet<>();
+        setMS(10, mathSetForJoin);
+        mathSetForJoin.print(helperMS);
+        mathSet.join(mathSetForJoin);
     }
 
     private void add(BufferedReader reader) {
-        System.out.print("Which element you want to add -> ");
-        try {
-            String character=reader.readLine();
-            Pattern p = Pattern.compile("\\d+");
-            Matcher m = p.matcher(character);
-            while(m.find()) {
-                mathSet.add(Double.parseDouble(m.group()));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        mathSet.print(currMS);
+        System.out.print("Which element you want to add (if element exist, then it won`t be added) -> ");
+        Scanner scanner = new Scanner(System.in);
+        mathSet.add(scanner.nextBigDecimal());
+        System.lineSeparator();
     }
 
     private void newMathSet(BufferedReader reader) {
         try {
-            mathSet = new MathSet<>();
+            System.out.println("Enter a count of elements in new math set (min 2): ");
             int countOfElements = Integer.parseInt(reader.readLine());
-            for (int i = 0; i < countOfElements; i++) {
-                Number random = Math.floor(Math.random() * (1000 - 1 + 1) + 1);
-                mathSet.add(random);
+            if (countOfElements >= 2) {
+                mathSet = new MathSet<>();
+                setMS(countOfElements, mathSet);
+            } else {
+                newMathSet(reader);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        mathSet.print(currMS);
+    }
+
+    private void setMS(int countOfElements, MathSet<Number> ms) {
+        for (int i = 0; i < countOfElements; i++) {
+            Number random = null;
+            if (i % 2 == 0) {
+                Random r = new Random();
+                random = BigDecimal.valueOf(r.nextInt(50 - 1) + 1);
+            } else {
+                Random r = new Random();
+                random = BigDecimal.valueOf(DoubleRounder.round(1 + (20 - 1) * r.nextDouble(), 2));
+            }
+            ms.add(random);
+        }
     }
 }
